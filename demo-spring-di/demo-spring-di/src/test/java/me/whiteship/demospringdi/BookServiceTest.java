a148256c093d562cb8088130bec49c276bb13cc5
TAEHONG <hong2wrd@gmail.com>
2023-03-03 15:13:28 +0900

    스프링 Dependency Injection은 어떻게 동작할까? - 내용 추가

--- demo-spring-di/demo-spring-di/src/test/java/me/whiteship/demospringdi/BookServiceTest.java
+++ demo-spring-di/demo-spring-di/src/test/java/me/whiteship/demospringdi/BookServiceTest.java
@@ -1,13 +1,12 @@
 package me.whiteship.demospringdi;
 
-import static org.junit.Assert.*;
-
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,6 +17,7 @@ public class BookServiceTest {
 
     @Test
     public void di() {
-        Assert.assertNotNull(bookService.bookRepository);
+        assertNotNull(bookService);
+        assertNotNull(bookService.bookRepository);
     }
 }
